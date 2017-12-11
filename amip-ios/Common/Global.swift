b82674fe2de1ad446b36430640c5196c37380281
aceplus <aceplus@aceplus0000.local>
2017-12-11 13:53:18 +0630

    Added to check Internet Connection

---
+++ amip-ios/Common/Global.swift
@@ -0,0 +1,20 @@
+//
+//  Global.swift
+//  amip-ios
+//
+//  Created by Administrator on 12/6/17.
+//  Copyright © 2017 Thuriya ACE Technology. All rights reserved.
+//
+
+import UIKit
+
+class Global {
+    
+    // Now Global.sharedGlobal is your singleton, no need to use nested or other classes
+    static let sharedGlobal = Global()
+    
+    
+    
+    var user:User?
+    
+}
